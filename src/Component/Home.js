2b12529a2bab9e31effde2fbab47721305c3737f
sachin <sachin@gmail.com>
2023-03-04 23:08:30 +0530

    New commit

---
+++ src/Component/Home.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import axios from "axios";
+function Home() {
+  const [data, setData] = useState("");
+  const [value, setValue] = useState("");
+  const [shortUrl, setShortUrl] = useState([]);
+  const handelClick = () => {
+    setValue(data);
+    setData("");
+    fetch(value);
+  };
+  const handelChange = (e) => {
+    setData(e.target.value);
+    //  console.log(e.target.value);
+  };
+
+  async function fetch(data) {
+    try {
+      const val = await axios(`https://api.shrtco.de/v2/shorten?url=${data}`);
+      setShortUrl([...shortUrl, val.data.result.full_short_link]);
+
+      axios.post(`https://urlshort-nea4.onrender.com`);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  return (
+    <>
+      <h1> URL shortner</h1>
+      <div>
+        <input type="text" value={data} onChange={handelChange}></input>
+        <button onClick={handelClick}>Submit</button>
+        {shortUrl &&
+          shortUrl.map((item, index) => {
+            return <h1>{item}</h1>;
+          })}
+      </div>
+    </>
+  );
+}
+
+export default Home;
---
+++ src/Component/Shortner.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+function Shortner({ inputValue }) {
+  return (
+    <>
+      <h1>Hello Shortner</h1>
+    </>
+  );
+}
+
+export default Shortner;
